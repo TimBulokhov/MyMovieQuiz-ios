6fe885221a453dbd6d704a20f8924f7b9c61cea0
Tim Bulokhov <timofeybrant@mail.ru>
2023-11-17 16:07:00 +0200

    Выполнено задание в конце 2 темы.

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+class AlertPresenter {
+    weak var mainView: UIViewController?
+    
+    init(mainView: UIViewController? = nil) {
+        self.mainView = mainView
+    }
+    
+    func newAlert(data: AlertModel) {
+        let alert = UIAlertController(
+            title: data.title,
+            message: data.message,
+            preferredStyle: .alert)
+        let action = UIAlertAction(title: data.buttonText, style: .default) { _ in
+            data.completion()
+        }
+        alert.addAction(action)
+        mainView?.present(alert, animated: true, completion: nil)
+    }
+}
