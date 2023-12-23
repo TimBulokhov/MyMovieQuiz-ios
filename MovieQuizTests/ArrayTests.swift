077be594a56d4760ec491ad409da680b3b9bcbd7
Tim Bulokhov <timofeybrant@mail.ru>
2023-12-23 12:39:44 +0200

    Написаны тесты для массивов - ArrayTests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -5,4 +5,21 @@
 //  Created by Timofey Bulokhov on 23.12.2023.
 //
 
-import Foundation
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws {
+        let array = [1,1,2,3,5]
+        let value = array[safe: 2]
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutRange() throws {
+        let array = [1, 1, 2, 3, 5]
+        let value = array[safe: 20]
+        XCTAssertNil(value)
+    }
+                  
+}
